d88106d3505b01332878d3d567b7fa7b010c800a
nickKearns <nickkearns133@gmail.com>
2019-10-01 19:56:09 -0700

    Users can edit, update, and destroy playlists

--- app.py
+++ app.py
@@ -17,7 +17,7 @@ def index():
 
 @app.route('/playlists/new')
 def playlist_new():
-    return render_template('playlists_new.html')
+    return render_template('playlists_new.html', playlist={}, title='New Playlists')
 
 
 
@@ -41,7 +41,23 @@ def playlists_show(playlist_id):
     playlist = playlists.find_one({'_id': ObjectId(playlist_id)})
     return render_template('playlists_show.html', playlist=playlist)
 
+@app.route('/playlists/<playlist_id>/edit')
+def playlists_edit(playlist_id):
+    """Show the edit form for a playlist."""
+    playlist = playlists.find_one({'_id': ObjectId(playlist_id)})
+    return render_template('playlists_edit.html', playlist=playlist, title='Edit Playlist')
 
+@app.route('/playlists/<playlist_id>', methods=['POST'])
+def playlists_update(playlist_id):
+    updated_playlist = {
+        'title': request.form.get('title'),
+        'description': request.form.get('description'),
+        'videos': request.form.get('videos').split()
+    }
+    playlists.update_one(
+        {'_id': ObjectId(playlist_id)},
+        {'$set': updated_playlist})
+    return redirect(url_for('playlists_show', playlist_id=playlist_id))
 
 if __name__ == '__main__':
     app.run(debug=True)
