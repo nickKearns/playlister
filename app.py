e0ceb1995d5faf08485dd66d5b8ff03e639f7487
nickKearns <nickkearns133@gmail.com>
2019-10-01 19:19:14 -0700

    Users can create playlists

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
+from bson.objectid import ObjectId
 from pymongo import MongoClient
 
 client = MongoClient()
@@ -7,22 +8,39 @@ playlists = db.playlists
 
 app = Flask(__name__)
 
-# OUR MOCK ARRAY OF PROJECTS
-# playlists = [
-#     { 'title': 'Cat Videos', 'description': 'Cats acting weird' },
-#     { 'title': '80\'s Music', 'description': 'Don\'t stop believing!' },
-#     { 'title': '90\'s Music', 'description': 'Smells like teen spirit'}
-# ]
-@app.route('/')
-def playlists_index():
-    '''Show all playlists'''
-    return render_template('playlists_index.html', playlists=playlists.find())
-
 
 @app.route('/')
 def index():
     """Return homepage."""
     return render_template('playlists_index.html', playlists=playlists.find())
+
+
+@app.route('/playlists/new')
+def playlist_new():
+    return render_template('playlists_new.html')
+
+@app.route('/playlists/<playlist_id>')
+def playlists_show(playlist_id):
+    """Show a single playlist."""
+    return f'My ID is {playlist_id}'
+
+
+@app.route('/playlists', methods=['POST'])
+def playlists_submit():
+    """Submit a new playlist."""
+    playlist = {
+        'title': request.form.get('title'),
+        'description': request.form.get('description'),
+        'videos': request.form.get('videos').split()
+    }
+    playlists.insert_one(playlist)
+    return redirect(url_for('playlists_index'))
+    
+@app.route('/playlists/<playlist_id>')
+def playlists_show(playlist_id):
+    """Show a single playlist."""
+    playlist = playlists.find_one({'_id': ObjectId(playlist_id)})
+    return render_template('playlists_show.html', playlist=playlist)
 
 if __name__ == '__main__':
     app.run(debug=True)
